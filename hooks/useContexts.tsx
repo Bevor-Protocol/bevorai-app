c472e2cb9fe3c1490f95503bb025bb58a783755a
Peter Simone <plssimone@gmail.com>
2025-09-25 12:05:13 -0400

    wip chat

--- hooks/useContexts.tsx
+++
@@ -1,5 +0,0 @@
-import { ChatContext } from "@/providers/chat";
-import { ChatContextType } from "@/utils/types";
-import { useContext } from "react";
-
-export const useChat = (): ChatContextType => useContext(ChatContext);
