4a75c60833cb5a89c3112b57fe96f6b1ad567fc4
Blake Hatch <blakewihatch@gmail.com>
2025-10-16 14:48:07 -0400

    Migrate metadata to support Base Mini App structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,7 @@ const baseURL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : "http://localhost:3000";
 
-export const metadata: Metadata = {
-
-};
+export const metadata: Metadata = {};
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -40,24 +38,25 @@ export async function generateMetadata(): Promise<Metadata> {
       creator: "@BevorAI_Agent",
       images: [`${baseURL}/opengraph.png`],
     },
-      other: {
+    other: {
       "fc:miniapp": JSON.stringify({
-          version: "next",
-          imageUrl: "https://pbs.twimg.com/profile_images/1886423225925533696/KRveUwhh_400x400.jpg",
-          button: {
-              title: "Bevor Mini App",
-              action: {
-                  type: "bevor_miniapp",
-                  name: "Bevor Mini App",
-                  url: "certaik-app-git-feat-base-mini-app-support-bevor.vercel.app",
-                  splashImageUrl: "https://pbs.twimg.com/profile_banners/1862890581896163328/1738593320/1500x500",
-                  splashBackgroundColor: "#000000",
-              },
+        version: "next",
+        imageUrl: "https://pbs.twimg.com/profile_images/1886423225925533696/KRveUwhh_400x400.jpg",
+        button: {
+          title: "Bevor Mini App",
+          action: {
+            type: "bevor_miniapp",
+            name: "Bevor Mini App",
+            url: "certaik-app-git-feat-base-mini-app-support-bevor.vercel.app",
+            splashImageUrl:
+              "https://pbs.twimg.com/profile_banners/1862890581896163328/1738593320/1500x500",
+            splashBackgroundColor: "#000000",
           },
+        },
       }),
-      },
-  };
-};
+    },
+  }
+}
 
 const RootLayout: AsyncComponent<{ children: React.ReactNode }> = async ({ children }) => {
   return (
